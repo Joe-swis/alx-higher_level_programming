4959ba0d901507dc7e0f2ccc9111b58a8ed011dd
Joe-swis <sitrachid8@gmail.com>
2025-11-05 23:13:39 +0100

    finish

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print("First 3 letters:",end=" ")
+print(f"{word[:3]}")
+print("Last 2 letters:",end=" ")
+print(f"{word[-2:]}")
+print("Middle word:",end=" ")
+print(f"{word[1:-1]}")
